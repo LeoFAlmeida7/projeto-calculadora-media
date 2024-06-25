16a46bf94466a010053f625b6a5917cc9430ad7c
LeoFAlmeida7 <leonardofalmeidavivo@gmail.com>
2024-06-25 12:35:27 -0300

    comitando a segunda vvez

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function atualizarMediaFinal(){
 
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('mediaFinalValor').innerHTML = mediaFinal;
+    document.getElementById('mediaFinalValor').innerHTML = mediaFinal.toFixed(2);
     document.getElementById('mediaFinalResultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado: spanReprovado;
 
 }
